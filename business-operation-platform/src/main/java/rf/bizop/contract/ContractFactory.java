4e72cada1367acda87b34b9d6db85c6a40b7512c
oasis-zhou <oasis.zhou@outlook.com>
2020-11-26 16:11:49 +0800

    remove fastjson

--- business-operation-platform/src/main/java/rf/bizop/contract/ContractFactory.java
+++ business-operation-platform/src/main/java/rf/bizop/contract/ContractFactory.java
@@ -1,9 +1,9 @@
 package rf.bizop.contract;
 
-import com.alibaba.fastjson.JSON;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
@@ -16,6 +16,7 @@ import org.web3j.tx.TransactionManager;
 import org.web3j.tx.gas.StaticGasProvider;
 import rf.foundation.context.AppContext;
 import rf.foundation.exception.GenericException;
+import rf.foundation.utils.JsonHelper;
 
 import java.math.BigInteger;
 
@@ -40,6 +41,8 @@ public class ContractFactory {
     private String password;
     @Value("${account.keystore}")
     private String keystore;
+    @Autowired
+    private JsonHelper jsonHelper;
 
     private static final StaticGasProvider gasProvider = new StaticGasProvider(GAS_PRICE,GAS_LIMIT);
 
@@ -129,12 +132,12 @@ public class ContractFactory {
         }
     }
 
-    public static String generateKeystore(String password){
+    public String generateKeystore(String password){
         String keystore = null;
         try {
             ECKeyPair ecKeyPair = Keys.createEcKeyPair();
             WalletFile walletFile = Wallet.createStandard(password, ecKeyPair);
-            keystore = JSON.toJSONString(walletFile);
+            keystore = jsonHelper.toJSON(walletFile);
 
         } catch (Exception e) {
             throw new GenericException(e);
